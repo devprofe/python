f148daa65ace3cd60a2134080f94bfa89bbb59ab
Ramon Vasquez <devprofe.git@gmail.com>
2020-06-16 22:22:21 -0400

    Solucion 002 Notas en FOR

--- 002_notasFor.py
+++ 002_notasFor.py
@@ -1,16 +1,20 @@
 #DECLARACION DE LISTA CON NOTAS
 notas = [2,5.5,2,4,4.5,2,6.5]
 suma = 0.0
+promedio = 0.0
 
 #MANEJO DE NOTAS MEDIANTE FOR
-for contenido in notas:
-    suma = contenido + suma
+for x in range(len(notas)):
+    suma = suma + notas[x]
 
 #ASIGNACION DE PROMEDIO EN ULTIMA POSICION
-notas.append(suma/len(notas))
+promedio = suma/len(notas)
+notas.append(promedio)
 
 #CONDICION DE APRUEBA O REPRUEBA
-if notas[7] >= 3.95:
-    print("ALUMNO APRUEBA CON : ",notas[7])
+if promedio >= 3.95:
+    print("NOTAS DEL ALUMNO: ",notas)
+    print("ALUMNO APRUEBA CON : ",promedio)
 else:
-    print("ALUMNO REPRUEBA CON : ",notas[7])
+    print("NOTAS DEL ALUMNO: ",notas)
+    print("ALUMNO REPRUEBA CON : ",promedio)
