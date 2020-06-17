f9a453d273e071ed8d7e633f77c6c4eec006bf10
Ramon Vasquez <devprofe.git@gmail.com>
2020-06-16 21:35:55 -0400

    Solucion 001 Funciones de Texto

---
+++ 001_funcionesTexto.py
@@ -0,0 +1,12 @@
+usuario = input("Ingrese Usuario: ")
+password = input("Ingrese Contraseña: ")
+if password.isalnum() and len(password) == 8:
+    print("Contraseña VALIDA")
+else:
+    print("Contraseña INVALIDA")
+
+#QUE HIZO MAL EL USUARIO
+if password.isalnum() == False:
+    print("USTED INGRESO ESPACIOS EN EL PASS")
+elif len(password) != 8:
+    print("USTED NO INGRESO 8 CARACTERES")
