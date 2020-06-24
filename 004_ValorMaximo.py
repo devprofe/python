a2277890b3ee34fa2a7573086d886a5295f5ff6c
Ramon Vasquez Toledo <66813529+devprofe@users.noreply.github.com>
2020-06-24 13:30:21 -0400

    Delete 004_ValorMaximo.py

--- 004_ValorMaximo.py
+++
@@ -1,6 +0,0 @@
-valores = [8555,41225,47844,674,544552,75,6,9955,23365]
-mayor = 0
-for x  in range(len(valores)):
-    if valores[x] > mayor:
-        mayor = valores[x]
-print("El Valor Mayor de la lista es: ", mayor)
